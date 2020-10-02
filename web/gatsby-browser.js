a5cd49c7daba16da8e5fd1c32b1b2217b2fd6e8f
Sander Geraedts <sander.geraedts@sogeti.com>
2020-10-02 18:58:10 +0200

    TEST: FOUC tegengaan

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,3 +4,9 @@ import Layout from "./src/containers/layout";
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
+
+export function onClientEntry() {
+  window.addEventListener("load", () => {
+    document.body.className = document.body.className.replace(/\bno-js\b/, "");
+  });
+}
--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -4,3 +4,9 @@ import Layout from "./src/containers/layout";
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
+
+export function onRenderBody({ setBodyAttributes }) {
+  setBodyAttributes({
+    className: "no-js",
+  });
+}
