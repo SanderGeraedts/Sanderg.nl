ef26cd9000cd7d9137ea53b3e23dc79cbf59f9c7
Sander Geraedts <sandergeraedts@live.nl>
2021-07-23 15:48:35 +0200

    Added screenshot upload to cloudinary

--- functions/open-graph.js
+++ functions/open-graph.js
@@ -1,10 +1,8 @@
 const chromium = require('chrome-aws-lambda');
+const cloudinary = require('cloudinary').v2; // Make sure to use v2
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, FAUNA_SECRET } = process.env;
 
-exports.handler = async (event, context) => {
-  const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
-  console.log(`Taking screenshot of ${pageToScreenshot}`);
-
-  console.log('creating browser...');
+const takeScreenshot = async (pageToScreenshot) => {
   const browser = await chromium.puppeteer.launch({
     executablePath: await chromium.executablePath,
     args: chromium.args,
@@ -12,26 +10,48 @@ exports.handler = async (event, context) => {
     headless: chromium.headless,
   });
 
-  console.log('creating page...');
-  console.log(browser);
   const page = await browser.newPage();
 
-  console.log(`navigating to ${pageToScreenshot}...`);
   await page.goto(pageToScreenshot);
 
-  console.log('taking screenshot...');
   const screenshot = await page.screenshot({ encoding: 'binary' });
 
-  console.log('closing browser...');
   await browser.close();
 
-  console.log(`Complete screenshot of ${pageToScreenshot}`);
+  return screenshot;
+};
 
+const uploadToCloudinary = async (screenshot) => {
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+  });
+
+  return new Promise((resolve, reject) => {
+    const uploadOptions = {};
+    cloudinary.uploader
+      .upload_stream(uploadOptions, (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      })
+      .end(screenshot);
+  });
+};
+
+exports.handler = async (event, context) => {
+  const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+
+  const screenshot = await takeScreenshot(pageToScreenshot);
+
+  const cloudinaryResponse = await uploadToCloudinary(screenshot);
+
+  console.log(`Created screenshot of ${pageToScreenshot}`);
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: `Complete screenshot of ${pageToScreenshot}`,
-      buffer: screenshot,
+      response: cloudinaryResponse,
     }),
   };
 };
