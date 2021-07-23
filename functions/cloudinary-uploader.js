d6fdcce566ead231908a4c57a5d214841749df85
Sander Geraedts <sandergeraedts@live.nl>
2021-07-23 16:11:20 +0200

    Splitted cloudinary uploader off into a seperate function, as the open graph function was getting to big. FaunaDB possible too big as well for open-graph function.

---
+++ functions/cloudinary-uploader.js
@@ -0,0 +1,33 @@
+const cloudinary = require('cloudinary').v2; // Make sure to use v2
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, FAUNA_SECRET } = process.env;
+
+const uploadToCloudinary = async (screenshot) => {
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+  });
+
+  return new Promise((resolve, reject) => {
+    const uploadOptions = {};
+    cloudinary.uploader
+      .upload_stream(uploadOptions, (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      })
+      .end(screenshot);
+  });
+};
+
+exports.handler = async (event, context) => {
+  const screenshot = JSON.parse(event.body).screenshot;
+
+  const cloudinaryResponse = await uploadToCloudinary(screenshot);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: cloudinaryResponse,
+    }),
+  };
+};
--- functions/open-graph.js
+++ functions/open-graph.js
@@ -1,6 +1,4 @@
 const chromium = require('chrome-aws-lambda');
-const cloudinary = require('cloudinary').v2; // Make sure to use v2
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, FAUNA_SECRET } = process.env;
 
 const takeScreenshot = async (pageToScreenshot) => {
   const browser = await chromium.puppeteer.launch({
@@ -22,20 +20,11 @@ const takeScreenshot = async (pageToScreenshot) => {
 };
 
 const uploadToCloudinary = async (screenshot) => {
-  cloudinary.config({
-    cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
-  });
-
-  return new Promise((resolve, reject) => {
-    const uploadOptions = {};
-    cloudinary.uploader
-      .upload_stream(uploadOptions, (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      })
-      .end(screenshot);
+  return await fetch(`/.netlify/functions/cloudinary-uploader`, {
+    method: 'POST',
+    body: JSON.stringify({
+      screenshot,
+    }),
   });
 };
 
@@ -44,7 +33,7 @@ exports.handler = async (event, context) => {
 
   const screenshot = await takeScreenshot(pageToScreenshot);
 
-  const cloudinaryResponse = await uploadToCloudinary(screenshot);
+  const cloudinaryResponse = await uploadToCloudinary(screenshot).data;
 
   console.log(`Created screenshot of ${pageToScreenshot}`);
   return {
