cdb98725e3fb0739f52aca3d64a67a03ec441613
Sander Geraedts <sander@totaldesign.com>
2023-04-25 20:57:35 +0200

    Updated from v0.20.3 to v2.3.1

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Sanderg.nl";
+export const SITE_DESCRIPTION =
+  "Hey, ik ben Sander Geraedts. Front-end Developer bij Total Design. maker, hardloper, en baasje van de veel te schattige puppy Luna.";
